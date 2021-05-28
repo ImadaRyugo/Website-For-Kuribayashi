b1d55580e1ca31c80b90411245b52e7f08d07acf
Ryugo <imadaryugo@gmail.com>
2021-05-28 23:45:15 +0900

    add fade-in animation

--- js/test.js
+++ js/test.js
@@ -11,4 +11,18 @@ function toggleNav() {
     });
   }
   toggleNav();
+  
 
+  let fadeInTarget = document.querySelectorAll('.fade-in');
+    
+  window.addEventListener('scroll', () => {
+    for (let i = 0; i < fadeInTarget.length; i++){
+    const rect = fadeInTarget[i].getBoundingClientRect().top;
+    const scroll = window.pageYOffset;
+    const offset = rect + scroll;
+    const windowHeight = window.innerHeight; // 現在のブラウザの高さ
+    if (scroll > offset - windowHeight + 150) {
+      fadeInTarget[i].classList.add('scroll-in');
+    }
+  }
+});
